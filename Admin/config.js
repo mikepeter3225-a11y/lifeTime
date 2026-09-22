5299abc219d5f212f83201dd584d7c47d64ac9e8
mikepeter3225-a11y <mikepeter3225@gmail.com>
2026-09-22 15:52:32 +0100

    Update config.js

--- Admin/config.js
+++ Admin/config.js
@@ -1,6 +1,6 @@
 // Global Configuration
 const CONFIG = {
-  API_BASE: 'http://localhost:5000/api',
+  API_BASE: 'https://life-time-seven.vercel.app/api',
   redirectLink: 'https://ebrparticipant.lh1ondemand.com/Login.aspx?ReturnUrl=%2f',
   botName: 'Lifetime Bot'
 };
